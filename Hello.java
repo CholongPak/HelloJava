5807fa00b5f10477023b96dadda98d3448ee82bb
Cholong Pak <113140296+CholongPak@users.noreply.github.com>
2022-09-15 22:02:49 -0500

    Hello Java

---
+++ Hello.java
@@ -0,0 +1,17 @@
+package sec03.exam01;
+
+/*
+ Creator: Cholong PAk
+ Date: Sep 15, 2022
+ */
+
+public class Hello {
+	//start point
+	public static void main(String[] args) {
+		//print
+		System.out.println("Hello, Java");
+		// TODO Auto-generated method stub
+
+	} //end of main
+
+} //end of class
